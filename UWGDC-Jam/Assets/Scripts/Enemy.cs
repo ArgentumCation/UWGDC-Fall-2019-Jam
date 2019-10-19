bd54ae0e1e129915684602ffbdfb7e772a09e245
Jacob van't Hoog <vantjac@gmail.com>
2019-10-18 19:13:29 -0700

    ZERGSSSS

---
+++ UWGDC-Jam/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Player target;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Component.FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toTarget = target.transform.position - transform.position;
+        toTarget = toTarget.normalized;
+        toTarget *= speed * Time.deltaTime;
+        transform.position += toTarget;
+    }
+}
---
+++ UWGDC-Jam/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        Vector3 vel = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        vel *= speed * Time.deltaTime;
+        transform.position += vel;
+    }
+}
