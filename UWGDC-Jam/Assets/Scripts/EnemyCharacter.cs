0034b22c6070880093dc7a92fb4493a6d8cb9352
Jacob van't Hoog <vantjac@gmail.com>
2019-10-19 12:16:58 -0700

    update enemycharacter script

--- UWGDC-Jam/Assets/Scripts/EnemyCharacter.cs
+++ UWGDC-Jam/Assets/Scripts/EnemyCharacter.cs
@@ -19,7 +19,9 @@ public class EnemyCharacter : EnemyClass
 
     private Vector3 VectorToTarget()
     {
-        Vector3 toTarget = (target.transform.position + RandomInCircle() * 2) - transform.position;
+        if (target == null)
+            return Vector3.zero;
+        Vector3 toTarget = (target.transform.position + RandomInCircle()) - transform.position;
         return toTarget.normalized;
     }
 
