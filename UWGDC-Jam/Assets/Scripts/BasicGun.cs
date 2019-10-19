9f4058cdaf68dc9239b86c480e8718f16f1d56af
Jacob van't Hoog <vantjac@gmail.com>
2019-10-18 20:41:52 -0700

    Start gun/bullets

---
+++ UWGDC-Jam/Assets/Scripts/BasicGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicGun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        var bullet = Instantiate(bulletPrefab);
+        var bulletBody = bullet.GetComponent<Rigidbody2D>();
+        bulletBody.transform.position = transform.position;
+        bulletBody.transform.rotation = transform.rotation;
+    }
+}
