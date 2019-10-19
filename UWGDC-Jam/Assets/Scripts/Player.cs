57cc5c4d7cff3fda958d1d2708b9a90633691e15
Jacob van't Hoog <vantjac@gmail.com>
2019-10-19 11:26:28 -0700

    Add mouse aiming

--- UWGDC-Jam/Assets/Scripts/Player.cs
+++ UWGDC-Jam/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public float speed;
     public float health = 100;
     private Rigidbody2D body;
+    private bool mouseAim = true;
 
     void Start()
     {
@@ -25,9 +26,18 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        if (move.sqrMagnitude > 1)
+            move = move.normalized;
         body.velocity = move * speed;
 
-        Vector2 aim = new Vector2(Input.GetAxis("Aim X"), Input.GetAxis("Aim Y"));
+        if (mouseAim && (Input.GetAxis("Aim X") != 0 || Input.GetAxis("Aim Y") != 0))
+            mouseAim = false;
+
+        Vector2 aim;
+        if (mouseAim)
+            aim = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        else
+            aim = new Vector2(Input.GetAxis("Aim X"), Input.GetAxis("Aim Y"));
         if (aim.magnitude > 0.01)
             transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.right, aim));
     }
